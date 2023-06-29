99e35d1d614d1e0f874481583d82c91d641b2cd2
unfaa3 <76907755+unfaa3@users.noreply.github.com>
2023-06-29 17:29:16 +0100

    add stew

--- mainPipeline.py
+++ mainPipeline.py
@@ -12,13 +12,48 @@ if __name__ == "__main__":
     # }
 
     '''Example for loading BCI Competition IV Dataset 2a'''
-    dataset_details={
-        'data_path' : "/Volumes/Transcend/BCI/BCICIV_2a_gdf",
-        'file_to_load': 'A01T.gdf',
+    # dataset_details={
+    #     'data_path' : "D:/2023/py_bath/BCI/data_set/BCICIV_2a_gdf",
+    #     'file_to_load': 'A01T.gdf',
+    #     'ntimes': 1,
+    #     'kfold':10,
+    #     'm_filters':2,
+    #     'window_details':{'tmin':0.5,'tmax':2.5}
+    # }
+    #
+    # ML_experiment = MLEngine(**dataset_details)
+    # ML_experiment.experiment()
+
+    '''CSP'''
+    # dataset_details={
+    #     'data_path' : "D:/2023/py_bath/BCI/data_set/BCICIV_2a_gdf",
+    #     'file_to_load': 'A01T.gdf',
+    #     'ntimes': 1,
+    #     'kfold':10,
+    #     'm_filters':2,
+    #     'window_details':{'tmin':0.5,'tmax':2.5}
+    # }
+
+    # dataset_details = {
+    #     'data_path': "D:/2023/py_bath/BCI/2021_code/SIDdataset",
+    #     'subject_id': 1,
+    #     'sessions': [1],
+    #     'file_to_load': 'dataset.mat',
+    #     'ntimes': 1,
+    #     'kfold': 10,
+    #     'm_filters': 2,
+    #     # 'window_details': {'tmin': 0.5, 'tmax': 2.5}
+    # }
+
+    dataset_details = {
+        'data_path': "D:/2023/py_bath/workload/my_code/stew",
+        'subject_id': 1,
+        'sessions': [1],
+        'file_to_load': 'stew_dataset.mat',
         'ntimes': 1,
-        'kfold':10,
-        'm_filters':2,
-        'window_details':{'tmin':0.5,'tmax':2.5}
+        'kfold': 10,
+        'm_filters': 2,
+        # 'window_details': {'tmin': 0.5, 'tmax': 2.5}
     }
 
     ML_experiment = MLEngine(**dataset_details)
